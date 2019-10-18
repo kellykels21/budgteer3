c63bddfa4502ae377856c9b7e6f80355375ea1be
Markel Spellman <Kels@Users-MacBook-Pro.local>
2019-10-18 15:10:26 -0400

    tooks transitions out of dependency

--- components/Slider.js
+++ components/Slider.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native'
-import CircleSlider from 'react-native-circle-slider';
+
 
 function Slider() {
   return (
-    <View>
-    </View>
+    <View></View>
   )
 }
 
--- utils/TransitionHandler.js
+++ utils/TransitionHandler.js
@@ -1,4 +1,4 @@
-import { fromLeft, fromRight } from 'react-navigation-transitions';
+import { Animated, Easing } from 'react-native';
 
 const handleCustomTransition = ({ scenes }) => {
   const prevScene = scenes[0] ? scenes[0] : undefined;
@@ -16,6 +16,60 @@ const handleCustomTransition = ({ scenes }) => {
   } else {
     return fromRight(700);
   }
+}
+
+function fromLeft(duration = 300) {
+  return {
+    transitionSpec: {
+      duration,
+      easing: Easing.out(Easing.poly(4)),
+      timing: Animated.timing,
+      useNativeDriver: true,
+    },
+    screenInterpolator: ({ layout, position, scene }) => {
+      const { index } = scene;
+      const { initWidth } = layout;
+
+      const translateX = position.interpolate({
+        inputRange: [index - 1, index, index + 1],
+        outputRange: [-initWidth, 0, 0],
+      });
+
+      const opacity = position.interpolate({
+        inputRange: [index - 1, index - 0.99, index],
+        outputRange: [0, 1, 1],
+      });
+
+      return { opacity, transform: [{ translateX }] };
+    },
+  };
+}
+
+function fromRight(duration = 300) {
+  return {
+    transitionSpec: {
+      duration,
+      easing: Easing.out(Easing.poly(4)),
+      timing: Animated.timing,
+      useNativeDriver: true,
+    },
+    screenInterpolator: ({ layout, position, scene }) => {
+      const { index } = scene;
+      const { initWidth } = layout;
+
+      const translateX = position.interpolate({
+        inputRange: [index - 1, index, index + 1],
+        outputRange: [initWidth, 0, 0],
+      });
+
+      const opacity = position.interpolate({
+        inputRange: [index - 1, index - 0.99, index],
+        outputRange: [0, 1, 1],
+      });
+
+      return { opacity, transform: [{ translateX }] };
+    },
+  };
 }
 
 export default handleCustomTransition
